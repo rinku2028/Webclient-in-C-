0c2227c7ba0467156591771fcde66d2d26080543
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-05-31 18:10:21 +0545

    Download HTML Tag from website

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //Download HTML Tags from webclient
+            WebClient client = new WebClient();
+            string store = client.DownloadString("https://www.worldometers.info/coronavirus/");
+            Console.WriteLine(store);
+            Console.ReadLine();
+        }
+    }
+}
